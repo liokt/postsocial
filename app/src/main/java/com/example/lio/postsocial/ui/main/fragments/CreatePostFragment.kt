f5c21d0d1725ec0b860b21e07c3ee2832a14c2df
Lio <liomazza@gmail.com>
2021-08-21 11:51:06 -0300

    add slide up animations

--- app/src/main/java/com/example/lio/postsocial/ui/main/fragments/CreatePostFragment.kt
+++ app/src/main/java/com/example/lio/postsocial/ui/main/fragments/CreatePostFragment.kt
@@ -15,6 +15,7 @@ import com.bumptech.glide.RequestManager
 import com.example.lio.postsocial.R
 import com.example.lio.postsocial.other.EventObserver
 import com.example.lio.postsocial.ui.main.viewmodels.CreatePostViewModel
+import com.example.lio.postsocial.ui.slideUpViews
 import com.example.lio.postsocial.ui.snackbar
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
@@ -70,6 +71,8 @@ class CreatePostFragment: Fragment(R.layout.fragment_create_post) {
                 viewModel.createPost(uri, etPostDescription.text.toString())
             } ?: snackbar(getString(R.string.error_no_image_chosen))
         }
+
+        slideUpViews(requireContext(), ivPostImage, btnSetPostImage, tilPostText, btnPost)
     }
 
     private fun subscribeToObservers() {
