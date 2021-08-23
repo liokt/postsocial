a87cb8594bdc22afd918e0eee6d7c10307bbc488
Lio <liomazza@gmail.com>
2021-08-23 19:19:52 -0300

    post interactions

--- app/src/main/java/com/example/lio/postsocial/ui/main/fragments/BasePostFragment.kt
+++ app/src/main/java/com/example/lio/postsocial/ui/main/fragments/BasePostFragment.kt
@@ -8,8 +8,10 @@ import androidx.fragment.app.Fragment
 import com.bumptech.glide.RequestManager
 import com.example.lio.postsocial.adapters.PostAdapter
 import com.example.lio.postsocial.other.EventObserver
+import com.example.lio.postsocial.ui.main.dialogs.DeletePostDialog
 import com.example.lio.postsocial.ui.main.viewmodels.BasePostViewModel
 import com.example.lio.postsocial.ui.snackbar
+import com.google.firebase.auth.FirebaseAuth
 import javax.inject.Inject
 
 abstract class BasePostFragment(
@@ -25,12 +27,63 @@ abstract class BasePostFragment(
     protected abstract val postProgressBar: ProgressBar
     protected abstract val basePostViewModel: BasePostViewModel
 
+    private var curLikedIndex: Int? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         subscribeToObservers()
+
+        postAdapter.setOnLikeClickListener { post, i ->
+            curLikedIndex = i
+            post.isLiked = !post.isLiked
+            basePostViewModel.toggleLikeForPost(post)
+        }
+
+        postAdapter.setOnDeletePostClickListener { post ->
+            DeletePostDialog().apply {
+                setPositiveListener {
+                    basePostViewModel.deletePost(post)
+                }
+            }.show(childFragmentManager, null)
+        }
     }
 
     private fun subscribeToObservers() {
+        basePostViewModel.likePostStatus.observe(viewLifecycleOwner, EventObserver(
+            onError = {
+                curLikedIndex?.let { index ->
+                    postAdapter.posts[index].isLiking = false
+                    postAdapter.notifyItemChanged(index)
+                }
+                snackbar(it)
+            },
+            onLoading = {
+                curLikedIndex?.let { index ->
+                    postAdapter.posts[index].isLiking = true
+                    postAdapter.notifyItemChanged(index)
+                }
+            }
+        ) { isLiked ->
+            curLikedIndex?.let { index ->
+                val uid = FirebaseAuth.getInstance().uid!!
+                postAdapter.posts[index].apply {
+                    this.isLiked = isLiked
+                    if(isLiked) {
+                        likedBy += uid
+                    } else {
+                        likedBy -= uid
+                    }
+                }
+                postAdapter.notifyItemChanged(index)
+            }
+        })
+
+        basePostViewModel.deletePostSatatus.observe(viewLifecycleOwner, EventObserver(
+            onError = { snackbar(it) }
+        ) {deletedPost ->
+            postAdapter.posts -= deletedPost
+        })
+
         basePostViewModel.posts.observe(viewLifecycleOwner, EventObserver(
             onError = {
                 postProgressBar.isVisible = false
