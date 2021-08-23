bd1250e551b182755613dc9fe579249012a30eae
Lio <liomazza@gmail.com>
2021-08-23 18:50:30 -0300

    home viewModel & home fragment

--- app/src/main/java/com/example/lio/postsocial/ui/main/viewmodels/BasePostViewModel.kt
+++ app/src/main/java/com/example/lio/postsocial/ui/main/viewmodels/BasePostViewModel.kt
@@ -27,7 +27,7 @@ abstract class BasePostViewModel(
     private val _likedByUsers = MutableLiveData<Event<Resource<List<User>>>>()
     val likedByUsers: LiveData<Event<Resource<List<User>>>> = _likedByUsers
 
-    abstract val post: LiveData<Event<Resource<List<Post>>>>
+    abstract val posts: LiveData<Event<Resource<List<Post>>>>
 
     abstract fun getPosts(uid: String = "")
 
